db43e1b4f6da2637e83efcf89ff25a5a1469f930
Thierry <varelathierry@gmail.com>
2023-07-25 17:38:20 -0300

    feat: avaliacao de visitas

---
+++ _1.2_Tipos_De_Dados_E_Estruturas_De_Controle_De_Fluxo/src/App.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class App {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int number = Input.menu(scanner);
+  }
+}
