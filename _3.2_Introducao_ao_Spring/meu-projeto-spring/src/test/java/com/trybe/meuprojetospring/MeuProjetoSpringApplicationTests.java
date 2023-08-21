b4974afb1b0c06c9c32c96460f8d8b97ba8d2d7e
Thierry <varelathierry@gmail.com>
2023-08-21 16:26:54 -0300

    feat: primeiro projeto spring

---
+++ _3.2_Introducao_ao_Spring/meu-projeto-spring/src/test/java/com/trybe/meuprojetospring/MeuProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trybe.meuprojetospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
